fb8c3390ab3868b6438aa0378a9d92dc7ed1fb15
josuevtl1337 <josuevtl1337@gmail.com>
2019-10-21 15:29:24 -0300

    testing

--- aplicacion.js
+++ aplicacion.js
@@ -45,9 +45,6 @@ $(document).ready(function() {
 
         video.currentTime = 0;
         video.play();
-      } else {
-        tl.fromTo(section, 0.5, { y: "50", opacity: 0 }, { y: 0, opacity: 1 });
-        console.log(destination.item);
       }
     }
   });
