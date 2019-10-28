5020a6a081904893ada061da1d8b132ceec64372
josuevtl1337 <josuevtl1337@gmail.com>
2019-10-28 09:19:19 -0300

    Falta hacer responsivo la comparacion

--- aplicacion.js
+++ aplicacion.js
@@ -30,7 +30,8 @@ $(document).ready(function() {
     scrollHorizontally: true,
     onLeave: (origin, destination) => {
       const tl = new TimelineMax({ delay: 0.5 });
-      const tl1 = new TimelineMax({ delay: 0.7 });
+      const tl1 = new TimelineMax({ delay: 0.5 });
+      const tl3 = new TimelineMax({ delay: 0.5 });
       const section = destination.item;
       const title = section.querySelector("p");
       const video = document.getElementById("videoleche");
@@ -42,6 +43,10 @@ $(document).ready(function() {
       var pattern = $(".descripz");
       var info = $(".cntinfo");
       var cajaLeche = $(".cntcaja");
+      const p1 = $("#parrafo1", "#parrafo2");
+      const p2 = $("#parrafo2");
+      const p3 = $("#parrafo3");
+      const p4 = $("#parrafo4");
 
       if (destination.index === 0) {
         // const tl = new TimelineMax({ delay: 0.5 });
@@ -61,14 +66,21 @@ $(document).ready(function() {
       } else if (destination.index === 1) {
         tl.fromTo(
           parrafos1,
-          0.3,
+          0.5,
           { x: "-200", opacity: 0 },
           { x: "0", opacity: 1 }
-        )
-          .fromTo(parrafos2, 0.3, { x: 200, opacity: 0 }, { x: 0, opacity: 1 })
-          .fromTo(foto, 0.7, { y: -20, opacity: 0 }, { y: 0, opacity: 1 })
+        );
+        tl1.fromTo(
+          parrafos2,
+          0.5,
+          { x: 200, opacity: 0 },
+          { x: 0, opacity: 1 }
+        );
+        tl3.fromTo(foto, 0.8, { y: -100, opacity: 0 }, { y: 0, opacity: 1 });
+        tl3
           .fromTo(globos, 0.8, { x: -200, opacity: 0 }, { x: 0, opacity: 1 })
-          .fromTo(pattern, 0.8, { opacity: 0 }, { opacity: 1 });
+          .fromTo(pattern, 0.8, { opacity: 0 }, { opacity: 1 })
+          .fromTo(p1, 0.8, { opacity: 0 }, { opacity: 1 });
       } else if (destination.index === 2) {
         tl.fromTo(cajaLeche, 0.5, { x: 400, opacity: 0 }, { x: 0, opacity: 1 });
         tl.fromTo(info, 0.5, { x: -400, opacity: 0 }, { x: 0, opacity: 1 });
@@ -82,7 +94,7 @@ $(document).ready(function() {
         );
         tl.fromTo(dha, 0.4, { y: "200", opacity: 0 }, { y: "0", opacity: 1 });
         // setTimeout("animandoIconos()", 5000);
-        var procesoId = window.setInterval(animandoIconos, "8000");
+        // var procesoId = window.setInterval(animandoIconos, "8000");
       } else {
         tl.fromTo(section, 0.5, { y: 0, opacity: 0 }, { y: 0, opacity: 1 });
         console.log(destination.item);
@@ -110,26 +122,26 @@ $(document).ready(function() {
     // destination.index = 1;
     alert("funciona");
   });
-  function animandoIconos() {
-    var random = Math.floor(Math.random() * 4 + 1);
-    console.log(random);
+  // function animandoIconos() {
+  //   var random = Math.floor(Math.random() * 4 + 1);
+  //   console.log(random);
 
-    if (random === 1) {
-      omega.css({
-        animation: "bounce 1s"
-      });
-    } else if (random === 2) {
-      chzc.css({
-        animation: "bounce 1s"
-      });
-    } else if (random === 3) {
-      vitaminas.css({
-        animation: "bounce 1s"
-      });
-    } else if (random === 4) {
-      dha.css({
-        animation: "bounce 1s"
-      });
-    }
-  }
+  //   if (random === 1) {
+  //     omega.css({
+  //       animation: "bounce 1s"
+  //     });
+  //   } else if (random === 2) {
+  //     chzc.css({
+  //       animation: "bounce 1s"
+  //     });
+  //   } else if (random === 3) {
+  //     vitaminas.css({
+  //       animation: "bounce 1s"
+  //     });
+  //   } else if (random === 4) {
+  //     dha.css({
+  //       animation: "bounce 1s"
+  //     });
+  //   }
+  // }
 });
