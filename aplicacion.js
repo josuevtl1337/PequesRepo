28ee779b3aed7f9a290232798bbb0528b7bf4aa1
josuevtl1337 <josuevtl1337@gmail.com>
2019-11-06 10:14:20 -0300

    cambiosdeultimahorahehexd

--- aplicacion.js
+++ aplicacion.js
@@ -2,13 +2,10 @@
 $(document).ready(function() {
   const onda = document.getElementById("onda");
   const leche = document.getElementById("caja");
-  var omega = $(".omega");
-  var chzc = $(".chzc");
-  var vitaminas = $(".vitaminas");
-  var dha = $(".dha");
   // const textos = document.getElementById("textoContenedor");
-  const textos = document.querySelectorAll("#miniatura, #textoContenedor");
-  const miniatura = document.getElementById("miniatura");
+  const textos = document.querySelectorAll(".miniatura, #textoContenedor");
+  const miniatura = $(".miniatura");
+  const vid = document.getElementById("videoNena");
   // const flecha = document.getElementById("flechaContenedor");
   // const svg = document.getElementById("flecha").getSVGDocument();
   const tl = new TimelineMax({ delay: 0.5 });
@@ -37,16 +34,15 @@ $(document).ready(function() {
       const video = document.getElementById("videoleche");
       const logo = document.getElementById("logo");
       var foto = $("#mejorcont");
-      var parrafos1 = $("#text1, #text2");
-      var parrafos2 = $("#text3, #text4");
+      var parrafos1 = $("#accordion");
+      var parrafos2 = $("#accordion2");
       var globos = $("#globos1");
       var pattern = $(".descripz");
       var info = $(".cntinfo");
       var cajaLeche = $(".cntcaja");
       const p1 = $("#parrafo1", "#parrafo2");
-      const p2 = $("#parrafo2");
-      const p3 = $("#parrafo3");
-      const p4 = $("#parrafo4");
+      const avion = $(".avion");
+      const nubes = $("#nube1 , #nube2, #nube3,#nube4,#nube5");
 
       if (destination.index === 0) {
         // const tl = new TimelineMax({ delay: 0.5 });
@@ -66,13 +62,13 @@ $(document).ready(function() {
       } else if (destination.index === 1) {
         tl.fromTo(
           parrafos1,
-          0.5,
+          0.6,
           { x: "-200", opacity: 0 },
           { x: "0", opacity: 1 }
         );
         tl1.fromTo(
           parrafos2,
-          0.5,
+          0.6,
           { x: 200, opacity: 0 },
           { x: 0, opacity: 1 }
         );
@@ -82,19 +78,48 @@ $(document).ready(function() {
           .fromTo(pattern, 0.8, { opacity: 0 }, { opacity: 1 })
           .fromTo(p1, 0.8, { opacity: 0 }, { opacity: 1 });
       } else if (destination.index === 2) {
-        tl.fromTo(cajaLeche, 0.5, { x: 400, opacity: 0 }, { x: 0, opacity: 1 });
-        tl.fromTo(info, 0.5, { x: -400, opacity: 0 }, { x: 0, opacity: 1 });
-        tl.fromTo(omega, 0.3, { y: "200", opacity: 0 }, { y: "0", opacity: 1 });
-        tl.fromTo(chzc, 0.4, { y: "200", opacity: 0 }, { y: "0", opacity: 1 });
-        tl.fromTo(
-          vitaminas,
-          0.4,
-          { y: "200", opacity: 0 },
-          { y: "0", opacity: 1 }
+        $(avion).animate(
+          {
+            opacity: 0
+          },
+          3500
         );
-        tl.fromTo(dha, 0.4, { y: "200", opacity: 0 }, { y: "0", opacity: 1 });
+        var lechecomun = $(".cntlecheComun");
+        tl.fromTo(cajaLeche, 0.8, { x: 400, opacity: 0 }, { x: 0, opacity: 1 });
+        tl1
+          .fromTo(
+            lechecomun,
+            0.8,
+            { x: -400, opacity: 0 },
+            { x: 0, opacity: 1 }
+          )
+          .fromTo(info, 1, { opacity: 0 }, { opacity: 1 });
+        var pilotoSolo = $(".rayo");
+        var piloto = $(".rayo , .cnticonos");
+        // var bandera = $(".bandera");
+        tl.fromTo(piloto, 1, { x: 400, opacity: 0 }, { x: 0, opacity: 1 })
+          .fromTo(nubes, 1, { opacity: 0 }, { opacity: 1 })
+          .fromTo(avion, 2, { x: -400, opacity: 0 }, { x: 0, opacity: 1 });
+        const tl4 = new TimelineMax({ delay: 6 });
+        tl4.fromTo(avion, 2, { x: 0, opacity: 1 }, { x: 400, opacity: 0 });
+        // .fromTo(pilotoSolo, 1, { x: 0, opacity: 1 }, { x: -400, opacity: 0 });
+        // .fromTo(bandera, 1, { x: -100, opacity: 0 }, { x: 0, opacity: 0.4 });
         // setTimeout("animandoIconos()", 5000);
         // var procesoId = window.setInterval(animandoIconos, "8000");
+
+        //Animaciones del avion
+        // const fligthPath = {
+        //   curviness: 1.25,
+        //   autoRotate: true,
+        //   values: [{ x: -400, y: 0 }, { x: 0, y: 0 }]
+        // };
+        // const tween = new TimelineLite();
+        // tween.add(
+        //   TweenLite.to(".avion", 1, {
+        //     bezier: fligthPath,
+        //     ease: Power1.easeInOut
+        //   })
+        // );
       } else {
         tl.fromTo(section, 0.5, { y: 0, opacity: 0 }, { y: 0, opacity: 1 });
         console.log(destination.item);
@@ -121,6 +146,16 @@ $(document).ready(function() {
   $(flecha).click(function() {
     // destination.index = 1;
     alert("funciona");
+  });
+  var play = $("#playbtn");
+  $(miniatura).hover(function() {
+    play.animate();
+  });
+
+  $(miniatura).on("click", function(e) {
+    console.log(vid);
+    e.preventDefault();
+    vid.requestFullscreen();
   });
   // function animandoIconos() {
   //   var random = Math.floor(Math.random() * 4 + 1);
@@ -167,4 +202,39 @@ $(document).ready(function() {
   //     morphing.play();
   //   });
   // });
+  /* Animaciones de los parrafos en la descripcion*/
+  $(function() {
+    $("#accordion").accordion({
+      collapsible: true,
+      active: false
+    });
+  });
+  $(function() {
+    $("#accordion2").accordion({
+      collapsible: true,
+      active: false
+    });
+  });
+
+  /*    Animacion de la INFO COMPLETA     */
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById("infonutric");
+  var btntrigger = document.getElementById("btninfo");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  btntrigger.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  };
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
 });
